367dc24a10bdc24bb667305eb520d13d4f1b8ac5
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-04-18 16:38:49 +0300

    feat(error): add umbrella header

---
+++ include/vix/error/error.hpp
@@ -0,0 +1,31 @@
+/**
+ *
+ *  @file error/error.hpp
+ *  @author Gaspard Kirira
+ *
+ *  @brief Internal aggregation header for the Vix error module.
+ *
+ *  This file includes all core components of the Vix error system.
+ *
+ *  For most use cases, prefer:
+ *    #include <vix/error.hpp>
+ *
+ *  Copyright 2025, Gaspard Kirira.
+ *  All rights reserved.
+ *  https://github.com/vixcpp/vix
+ *
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  Vix.cpp
+ */
+#ifndef VIX_ERROR_ERROR_HPP
+#define VIX_ERROR_ERROR_HPP
+
+#include <vix/error/Error.hpp>
+#include <vix/error/ErrorCategory.hpp>
+#include <vix/error/ErrorCode.hpp>
+#include <vix/error/Exception.hpp>
+#include <vix/error/Result.hpp>
+
+#endif // VIX_ERROR_ERROR_HPP
